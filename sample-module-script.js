b7fd34389e2baf4437dfcc03198ad85fcf24216a
marvinaglubo <58284059+marvinaglubo@users.noreply.github.com>
2024-08-09 14:31:56 +0800

    Update sample-module-script.js

--- sample-module-script.js
+++ sample-module-script.js
@@ -1,5 +1,3 @@
-import {initializeApp as fbInitialize} from 'test'
 import {initializeApp} from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js'
 
 console.log('external-module-with-url-import', {initializeApp: initializeApp})
-console.log('external-module-with-mapped-import', {initializeApp: fbInitialize})
